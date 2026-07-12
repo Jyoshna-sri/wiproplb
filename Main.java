0699af183daed7c6710db3601c0431053b4ccb38
Jyoshna-sri <jyoshnasri1406@gmail.com>
2026-07-12 21:51:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Optional;
+
+class EmployeeDetails {
+    private int id;
+    private String name;
+
+    EmployeeDetails(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("Employee Id: " + id);
+        System.out.println("Employee Name: " + name);
+    }
+}
+
+class InvalidEmployeeException extends Exception {
+    InvalidEmployeeException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeDetails emp = null;
+
+        try {
+            EmployeeDetails e = Optional.ofNullable(emp)
+                    .orElseThrow(() -> new InvalidEmployeeException("Invalid Employee"));
+
+            e.display();
+
+        } catch (InvalidEmployeeException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
