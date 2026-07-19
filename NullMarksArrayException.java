3b9c64029842440ec2e29d23bc7ba6b8a43bad3e
Jyoshna-sri <jyoshnasri1406@gmail.com>
2026-07-19 10:44:35 +0530

    Add files via upload

---
+++ NullMarksArrayException.java
@@ -0,0 +1,10 @@
+package com.mile1.exception;
+
+public class NullMarksArrayException extends Exception {
+   public NullMarksArrayException() {
+   }
+
+   public String toString() {
+      return "mark array is null";
+   }
+}
